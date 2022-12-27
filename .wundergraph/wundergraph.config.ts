38c395ff4e9770e0b2ddc969859ac65657a91df2
Dan <dan@example.com>
2022-12-27 12:04:23 -0500

    init

---
+++ .wundergraph/wundergraph.config.ts
@@ -0,0 +1,19 @@
+import { configureWunderGraphApplication, cors, EnvironmentVariable, introspect, templates } from '@wundergraph/sdk';
+import server from './wundergraph.server';
+import operations from './wundergraph.operations';
+
+const db = introspect.sqlserver({
+	apiNamespace: 'db',
+	databaseURL: process.env.SQL_CONN_STR || 'opps, you should set env SQL_CONN_STR',
+	introspection: {
+	  pollingIntervalSeconds: 5,
+	},
+});
+  
+configureWunderGraphApplication({
+	apis: [db],
+	server,
+	security: {
+		enableGraphQLEndpoint: true,
+	},
+});
